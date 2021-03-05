628f6896f28ca191196489b9cb3bef338418c4b7
madatoo <magda.ruszaj@gmail.com>
2021-03-05 16:25:15 +0000

    design form and selection boxinside form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,4 +3,6 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"}); //navbar initialization
     $('.slider').slider({height: 500}); //Slider initialization
+    $('select').formSelect(); //select form initalization
 });
+
