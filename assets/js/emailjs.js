7987940120bea3a4f93c978e4da41fdd78982be5
Fabio De Araujo <fabioaraujo76@gmail.com>
2021-03-07 22:01:57 +0000

    removed emailjs script from main js script and put it to run only on pages with the form to send, card pages

---
+++ assets/js/emailjs.js
@@ -0,0 +1,22 @@
+//Email JS send code from their documentation
+
+const btn = document.getElementById('send');
+
+document.getElementById('paddyform')
+.addEventListener('submit', function(event) {
+
+   event.preventDefault();
+
+   const serviceID = 'service_hmurxxg';
+   const templateID = 'template_ywblt9l';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+        M.toast({html: 'Your message was sent successfuly!', classes: "green darken-1"});
+        document.getElementById("paddyform").reset();
+    }, (err) => {
+        M.toast({html: `Sorry, we had trouble in sending your message. Error: ${err}`, classes: "red darken-1"});
+        document.getElementById("paddyform").reset();
+    });
+
+});
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,26 +5,3 @@ $(document).ready(function(){
     $('.slider').slider({height: 500}); //Slider initialization
     $('select').formSelect(); //select form initalization
 });
-
-
-//Email JS send code from their documentation
-
-const btn = document.getElementById('button');
-
-document.getElementById('paddyform')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   const serviceID = 'service_hmurxxg';
-   const templateID = 'template_ywblt9l';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-        M.toast({html: 'Your message was sent successfuly!', classes: "green darken-1"});
-        document.getElementById("paddyform").reset();
-    }, (err) => {
-        M.toast({html: `Sorry, we had trouble in sending your message. Error: ${err}`, classes: "red darken-1"});
-        document.getElementById("paddyform").reset();
-    });
-
-});
